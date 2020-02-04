c87d1d9175496281582e9f3382a0cc8478ea5179
NeoDragon <mcvickerstephen@gmail.com>
2020-02-04 21:55:31 +0000

    Updates

---
+++ precache-manifest.fec71eaf1bf6c562f6ef9e10ad83e211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cde750814193c973ca48c5eb0819b7",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "1c39814fc563e4eac0bf",
+    "url": "/react-weather-app/static/css/main.20c9ea1c.chunk.css"
+  },
+  {
+    "revision": "7360854b630e0fd59eaf",
+    "url": "/react-weather-app/static/js/2.f59c4712.chunk.js"
+  },
+  {
+    "revision": "ac3f8e58b5796b271331f2926a0e8b61",
+    "url": "/react-weather-app/static/js/2.f59c4712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c39814fc563e4eac0bf",
+    "url": "/react-weather-app/static/js/main.5df3eb1d.chunk.js"
+  },
+  {
+    "revision": "3d94d53cd9abf613c064",
+    "url": "/react-weather-app/static/js/runtime-main.d0ad0527.js"
+  }
+]);
